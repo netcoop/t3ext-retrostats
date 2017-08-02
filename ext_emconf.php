33f2f7f8b09a936c648ee07575558c89f5243c98
©lemens <BastianBalthasarBux@users.noreply.github.com>
2017-08-02 11:16:33 +0200

    [TASK] Raise Version Information

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.1.1',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.2.0-7.9.99',
+			'typo3' => '7.6.21-8.7.99',
 		),
 		'conflicts' => array(
 		),
